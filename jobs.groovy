594c1c36861f83484fa241c03566cc5d3d1770c3
Mohit Sethi <mohitsethi@users.noreply.github.com>
2020-05-03 12:23:09 +0530

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -113,3 +113,27 @@ freeStyleJob('master6') {
         shell('echo "test"')
     }
 }
+
+
+freeStyleJob('master7') {
+    description("Do not change this config directly.")
+    // jdk("JDK 1.7.0_67")
+    label 'neoskill'
+    wrappers {
+      timestamps()
+    }
+    logRotator(-1, 10, 5, -1)
+    scm {
+        git {
+            remote {
+                url("https://github.com/mohitsethi/neoskills")
+            }
+            branch("master")
+            extensions {wipeOutWorkspace()}
+        }
+    }
+
+    steps {
+        shell('echo "test"')
+    }
+}
